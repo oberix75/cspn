de4c3122e6f83470f89654f5f282136913815396
Kilian <kilian@face.home>
2020-10-29 22:40:41 +0100

    XOR/Perm corrections

--- cspn.c
+++ cspn.c
@@ -61,11 +61,11 @@ void main(int argc, int argv[])
         // Fisrt XOR
         cipher1 = Entry ^ key;
         Sbox1 = ~(cipher1, SubBits);
-        Pbox1 = Sbox1 >> PerBits;
+        Pbox1 = Sbox1 & PerBits;
         // First Round
         cipher2 = Pbox1 ^ key;
         Sbox2 = ~(cipher2, SubBits);
-        Pbox2 = Sbox2 >> PerBits;
+        Pbox2 = Sbox2 & PerBits;
         //Second Round
         cipher3 = Pbox2 ^ key;
         AND = cipher3 & key;
@@ -73,10 +73,10 @@ void main(int argc, int argv[])
         ciphertext = AND ^ key;
         // Ciphertext
         
-        printf("This is your ciphertext with SPN : %i", ciphertext);
+        printf("This is your ciphertext : %i\n", ciphertext);
     }
     else
     {
-        printf ("Usage : First Argument is the Plaintext, Second Argument is the Key, Third Argument is the Initialization vector, Fourth Argument is the Substitution Integer and Fifth Argument is the Permutation Integer");
+        printf ("Usage : First Argument is the Plaintext, Second Argument is the Key, Third Argument is the Initialization vector, Fourth Argument is the Substitution Integer and Fifth Argument is the Permutation Integer\n");
     }
 }
